a80e2e809c04acc3e32f16cebc8222a0b06ff1f6
Alkis Mavridis <alkismavridis@gmail.com>
2021-09-12 08:47:56 +0200

    Remove window state

--- src/main/ts/gui/index.ts
+++ src/main/ts/gui/index.ts
@@ -2,6 +2,10 @@ import FooApp from "./components/FooApp/FooApp";
 import Renderix from "./renderix/api/Renderix";
 import  "./css/index.scss";
 
-// Define necessary global scope fields
-(window as any).FooApp = FooApp;
-(window as any).Renderix = Renderix;
+
+window.addEventListener("load", () => {
+  Renderix.mount(
+    document.querySelector("body"),
+    [[FooApp]]
+  );
+})
